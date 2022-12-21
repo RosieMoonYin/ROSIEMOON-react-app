f2501e351db6edc50359fcb96891fff0cef415fc
Rosie Moon <rosie.victoria.burnett@gmail.com>
2022-12-21 20:21:27 +0100

    new updates

--- creative-bio/src/components/About.js
+++ creative-bio/src/components/About.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
+import JournalList from "./JournalList";
 
 const About = () => {
-   return (  
-    <>
-    <h2>Blog.</h2>
-    <h3>Merging with machine, digital consciousness entanglement. Where do we go from here?</h3>
-    <p>More things about</p>
-    </>
-   );
-};
+  const [Journal, setJournal] = useState([
+    { title: 'Merging with machine', body: 'lorem ipsum...', author: 'RosiemoonYin', id: 1 },
+    { title: 'The issue with ransome attacks', body: 'lorem ipsum...', author: 'RosiemoonYin', id: 2 },
+    { title: 'Turing Test, fooling the human', body: 'lorem ipsum...', author: 'RosiemoonYin', id: 3 }
+  ])
+
+  return (
+    <div className="about">
+        <JournalList blogs={Journal}/>
+    </div>
+  );
+}
 
 export default About;
 
